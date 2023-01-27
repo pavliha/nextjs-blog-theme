47dc49e08273d1b0281e07cd9f3199d8b8b0e778
Pavlo Kostiuk <kostyk.pavel.09@gmail.com>
2023-01-27 20:31:11 +0400

    add typescript

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  swcMinify: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    // ignoreBuildErrors: true,
+  },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+};
+export default config;
